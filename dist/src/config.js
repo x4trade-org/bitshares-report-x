ae513c84d806a615fc6e67852472c6c14f3e9068
x4trade <x4trade@glibx.com>
2019-08-19 20:48:34 -0400

    init

---
+++ dist/src/config.js
@@ -0,0 +1,8 @@
+"use strict";
+
+module.exports = {
+    apiNode: "wss://node-tokyo.x4trade.org:9090", // wss://node-tokyo.x4trade.org:9090 is an alternative
+    useES: true, // use elastic search
+    esNode: "https://eswrapper.x4trade.org",
+    botPaymentAccounts: []
+};
---
+++ es/src/config.example.js
@@ -0,0 +1,6 @@
+module.exports = {
+    apiNode: "wss://node-tokyo.x4trade.org:9090", // wss://node-tokyo.x4trade.org:9090 is an alternative
+    useES: true, // use elastic search
+    esNode: "https://eswrapper.x4trade.org",
+    botPaymentAccounts: []
+};
